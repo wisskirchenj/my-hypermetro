968061f084960651b6fda2cbef80a0c66a72deba
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-06-07 23:29:57 +0200

    stage 3 passed - transfer stations are json-array (list in model) now.

--- src/main/java/de/cofinpro/metro/model/Station.java
+++ src/main/java/de/cofinpro/metro/model/Station.java
@@ -2,17 +2,31 @@ package de.cofinpro.metro.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * DTO representing a station in a metro line, given by the station name and a possible transfer station in
- * another line.
+ * DTO representing a station in a metro line, given by the station name and a possibly empty list of transfer stations
+ * of other lines.
  */
 @Data
 public class Station {
     private final String name;
-    private TransferStation transfer;
+    private List<TransferStation> transfer = new ArrayList<>();
 
     @Override
     public String toString() {
-        return name + (transfer == null ? "" : " - " + transfer);
+        StringBuilder builder = new StringBuilder(name);
+        transfer.forEach(transferStation -> builder.append(" - ").append(transferStation.getStation())
+                .append(" (").append(transferStation.getLine()).append(" line)"));
+        return builder.toString();
+    }
+
+    /**
+     * add the given transfer station to the transfer list
+     * @param transferStation the transfer station to add
+     */
+    public void addTransfer(TransferStation transferStation) {
+        transfer.add(transferStation);
     }
 }
